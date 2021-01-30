5f24137139ccbd6270a5cb9ba8251e0da9052601
rianbloger <rianbloger2@gmail.com>
2021-01-30 11:17:50 +0700

    commit pertama

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,8 @@
+<!-- need to remove -->
+<li class="nav-item">
+     <a href="{{ route('home') }}" class="nav-link active">
+         <i class="nav-icon fas fa-home"></i>
+         <p>Home</p>
+     </a>
+ </li>
+ 
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row mt-3 pt-5 "  >
+     <div class="col-md-12 p-5  bg-white">
+         <h1 class=" font-weight-bold " style="font-size: 36px">
+             WELCOME
+             
+         </h1>
+     </div>
+ </div>
+@endsection
